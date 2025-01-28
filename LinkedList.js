ce02ee48b2d5ed75277b8887ad3dbe22d02e48f8
Damian X <dx4336969@gmail.com>
2025-01-28 21:49:00 +0000

    implementing get, set,remove and reverse in linkedList

--- LinkedList.js
+++ LinkedList.js
@@ -5,7 +5,6 @@ class Node {
     }
 }
 
-
 class LinkedList {
     constructor(value){
         const newNode = new Node(value)
@@ -15,11 +14,10 @@ class LinkedList {
     }
 
     push(value){
-        const newNode = new Node(2)
-        if(!this.head){
+        const newNode = new Node(value)
+        if(!this.head) {
             this.head = newNode
             this.tail = newNode
-            
         }else{
             this.tail.next = newNode
             this.tail = newNode
@@ -27,34 +25,31 @@ class LinkedList {
         this.length++
         return this 
     }
-
     pop(){
         if(!this.head) return undefined
         let temp = this.head
         let pre = this.head
-        while(this.head.next){
-            pre = temp 
-            temp = this.head.next
+        
+        while(temp.next){
+            pre = temp
+            temp = temp.next
         }
         this.length--
-        if(this.length===0){
+        if(this.length === 0){
             this.head = null
-            this.tail = null
+            this.tail  = null
+            
         }
         return temp
     }
+
     unshift(value){
         const newNode = new Node(value)
-        
-        
         if(!this.head){
             this.head = newNode
             this.tail = newNode
-            
         }else{
-            
             newNode.next = this.head
-            
             this.head = newNode
         }
         this.length++
@@ -64,15 +59,78 @@ class LinkedList {
         if(!this.head) return undefined
         let temp = this.head
         this.head = this.head.next
-        this.length--
-        if(this.length===0){
+        temp.next = null
+        this.length --
+        if(this.length === 0){
             this.head = null
             this.tail = null
         }
-        return temp
+        return temp 
+    }
+
+    get(index){
+        if(index>this.length || index<0) return undefined
+        let temp =  this.head
+        for(let x = 0 ; x<index; x++){
+            temp = temp.next
+            
+        }
+        return temp 
+    }
+
+    set(index,value){
+        if(index<0 || index>this.length) return undefined
+        let temp = this.get(index)
+        temp.value = value
+        return true
+    }
+    inset(index,value){
+        if(!value) return undefined
+        if(index<0 || index>this.length) return undefined
+        if(index == 0) return this.unshift(value)
+        if(index == this.length) return this.push(value)
+        const newNode = new Node(value)
+        let temp = this.get(index-1)
+
+        newNode.next = temp.next
+        temp.next = newNode
+        this.length++
+        return true 
         
+        
+        
+    }
+    remove(index){
+        if(index<0 || index>this.length) return undefined
+        if(index === 0) return this.shift()
+        if(index ===this.length) return this.pop()
+        let pre = this.get(index-1)
+        let temp = this.get(index)
+        pre.next = temp.next
+        temp.next = null 
+        this.length--
+        return true
+        
+    }
+
+    reverse(){
+        if(!this.head) return undefined
+        let temp = this.head
+        let pre = null
+        let next = temp.next
+        this.head = this.tail
+        this.tail = temp 
+        for(let x = 0 ; x<this.length;x++){
+            next = temp.next
+            temp.next  = pre
+            pre = temp 
+            temp = next
+            
+        }
+        return this 
     }
 }
 
+const myLinkedList = new LinkedList(11)
 
-const myLinkedList  = new  LinkedList(3)
+

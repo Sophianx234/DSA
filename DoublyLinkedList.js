7b9fb3b742ad24ffaf76cb7dc781b2f9b6e7046b
Damian X <dx4336969@gmail.com>
2025-01-29 23:02:17 +0000

    implementing doubly linked list

---
+++ DoublyLinkedList.js
@@ -0,0 +1,48 @@
+class Node {
+    constructor(value){
+        this.value = value 
+        this.next = null
+        this.prev = null
+    }
+}
+
+
+class DoublyLinkedList {
+    constructor(value){
+        const newNode = new Node(value)
+        this.head = newNode
+        this.tail = newNode
+        this.length = 1
+        
+    }
+    push(value){
+        const newNode  = new Node(value)
+        if(!this.head){
+            this.head = newNode
+            this.tail = newNode
+            
+        }else{
+            this.tail.next = newNode
+            newNode.prev = this.tail
+            this.tail = newNode
+            
+        }
+        this.length++
+        return this
+    }
+    pop(){
+        let temp = this.tail 
+        this.tail = temp.prev
+        this.tail.next = null
+        temp.prev = null
+        this.length--
+        if(this.length === 0){
+            this.head = null
+            this.tail = null
+        }
+        return temp
+    }
+}
+
+
+const myDLList = new DoublyLinkedList(10)
